ed19e77daa97a2b0a78ea6c8ad27c8791355eeab
Douglas Braga <douglasmartinsbraga@gmail.com>
2020-03-21 20:55:26 +0000

    Pequenas melhorias

--- conf.sh
+++ conf.sh
@@ -3,7 +3,7 @@ anaconda = "Anaconda3-2019.10-Linux-x86_64.sh"
 wget "https://repo.anaconda.com/archive/"+anaconda
 chmod anaconda +777
 sh anaconda -b
-conda install numpy pandas scikit-learn jupyterlab r rstudio r-tidyverse -y
+conda install numpy pandas scikit-learn jupyterlab r-essentials r-tidyverse rstudio -y
 
 
 #Adicionar PPA do chorme
@@ -12,9 +12,19 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 
 #Atualiza o repositorio e instala pacotes
 sudo apt update 
-sudo snap install onlyoffice-desktopeditors docker 
-sudo snap install code --clasic
+sudo snap install onlyoffice-desktopeditors docker slack spotify
+sudo snap install code --classic
 sudo apt install gnome-tweak-tool google-chrome-stable git build-essential chrome-gnome-shell -y
+
+git config --global user.email "douglasmartinsbraga@gmail.com"
+git config --global user.name "Douglas Braga"
+
+code --install-extension eamodio.gitlens
+code --install-extension ms-python.python
+code --install-extension PKief.material-icon-theme
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension christian-kohler.path-intellisense
+code --install-extension VisualStudioExptTeam.vscodeintellicode
 
 #utilizar docker sem sudo
 sudo snap connect docker:home
@@ -28,3 +38,4 @@ sudo snap enable docker
 sudo apt dist-upgrade
 sudo apt autoremove
 
+# Dash to Panel, Caffeine, Soft brightness
