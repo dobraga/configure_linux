ad5d56654a8fff318f5f831c9e331c12750186b9
Douglas Braga <douglasmartinsbraga@gmail.com>
2022-03-27 18:55:51 -0300

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -17,21 +17,14 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 rm -f ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
-rm -f ~/.config/lvim/config.lua
-ln -s ~/dotfiles/config.lua ~/.config/lvim/config.lua
-
 rm -rf ~/.config/terminator
 mkdir ~/.config/terminator
 ln -s ~/dotfiles/config.terminator ~/.config/terminator/config
-
-rm -rf ~/.config/lvim/lsp-settings
-mkdir ~/.config/lvim/lsp-settings
 
 # Configura GNOME
 # https://askubuntu.com/questions/26056/where-are-gnome-keyboard-shortcuts-stored
 # dconf dump '/' > ~/dotfiles/custom-dconf.toml
 dconf load '/' < ~/dotfiles/custom-dconf.toml
-
 
 # Configura ZSH
 sudo chsh -s $(which zsh)
