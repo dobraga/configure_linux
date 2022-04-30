b0b4351288cfc811420b47b4490c0b71e0c10c01
Douglas Braga <douglasmartinsbraga@gmail.com>
2022-04-30 17:51:44 -0300

    update version doccker compose

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sudo apt install \
 cargo install bat exa ytop
 
 # Instala docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.3.4/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
